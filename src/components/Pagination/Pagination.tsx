2d5f97165da3e0728fb04f2d7e26e33d4079011c
Zhekich123 <axelb380@gmail.com>
2023-11-17 04:48:50 -0800

    blablabla

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,44 @@
-export const Pagination = () => {};
+import { getNumbers } from '../../utils';
+
+const items = getNumbers(1, 42)
+  .map(n => `Item ${n}`);
+
+export const Pagination = ({ total, perPage, currentPage }) => {
+  return (
+    <div>
+      <ul className="pagination">
+        <li className="page-item disabled">
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled="true"
+          >
+            «
+          </a>
+        </li>
+        {}
+        <li className="page-item active">
+          <a data-cy="pageLink" className="page-link" href="#1">1</a>
+        </li>
+        <li className="page-item">
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled="false"
+          >
+            »
+          </a>
+        </li>
+      </ul>
+      <ul>
+        <li data-cy="item">Item 1</li>
+        <li data-cy="item">Item 2</li>
+        <li data-cy="item">Item 3</li>
+        <li data-cy="item">Item 4</li>
+        <li data-cy="item">Item 5</li>
+      </ul>
+    </div>
+  );
+};
